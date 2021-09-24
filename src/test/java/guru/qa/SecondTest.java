b963d1c209750fbf9ab65ec741569f3961610dce
dtuchs <d.tuchs@propellerads.net>
2021-09-24 20:03:31 +0300

    Added second test

---
+++ src/test/java/guru/qa/SecondTest.java
@@ -0,0 +1,15 @@
+package guru.qa;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SecondTest {
+
+
+    @Test
+    void secondTest() {
+        Assertions.assertTrue(false);
+    }
+
+
+}
